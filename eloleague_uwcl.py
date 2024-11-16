ee10afefb2fe6de71cb5f546f46eee389e76bf8d
H. Moerk <77638622+GarlickWitch@users.noreply.github.com>
2024-11-16 10:03:59 +0100

    some terminology

--- eloleague_uwcl.py
+++ eloleague_uwcl.py
@@ -5,14 +5,14 @@ import pandas
 # all approved uwcl matches
 df = pandas.read_excel("data/uwcl.ods", engine="odf")
 
-# make a list of all possible teams in the data set
-allteams = set(df['n_HomeTeamID'].tolist() + df['n_AwayTeamID'].tolist())
+# make a list of all games in the data set
+allgames = set(df['n_HomeTeamID'].tolist() + df['n_AwayTeamID'].tolist())
 
 # all time league and few samples so unusually high k
 eloLeague = Elo(k = 60)
 
-for team in allteams:
-	eloLeague.addPlayer(team)
+for game in allgames:
+	eloLeague.addPlayer(game)
 
 for game in df.iterrows():
     # swap points based on result
@@ -24,6 +24,7 @@ for game in df.iterrows():
         eloLeague.gameOver(game[1]['n_AwayTeamID'], game[1]['n_HomeTeamID'], False)
  
 # rank based on elo points
-v = sorted(eloLeague.ratingDict.items(), key=lambda item: item[1])
+v = sorted(eloLeague.ratingDict.items(), key=lambda item: item[1], reverse=True)
 
+# trouble here is; each teamID is listed with more than one name in the match (c_HomeTeam, c_AwayTeam). For clarity it should print the most recent name.
 print(v)
